47f966183bf675afe2730b058453e7ecf6b60c24
yaseminutkueri <yaseminutkueri@sabanciuniv.edu>
2023-03-29 14:49:59 +0200

    canu assembly qc with quast

---
+++ scripts/quast_assembly_evaluation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core -n 1
+#SBATCH -t 00:30:00
+#SBATCH -o /home/yutkueri/private/slurm_messages/quast_eval.out
+#SBATCH -e /home/yutkueri/private/slurm_messages/quast_eval.err
+#SBATCH -J quast_eval
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --mail-user yasemin.utkueri.3540@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load quast/5.0.2
+
+assembly_file=/home/yutkueri/private/assembly/pacbio.contigs.fasta
+out_dir=/home/yutkueri/private/quality_checks/canu_assembly
+
+quast.py ${assembly_file} -o ${out_dir} 
