2618c99312d14f578d55d9d9950c0b30b9252b5d
yaseminutkueri <yaseminutkueri@sabanciuniv.edu>
2023-04-10 19:25:17 +0200

    DEG script

--- scripts/deg.sh
+++ scripts/deg.sh
@@ -13,8 +13,12 @@
 module load bioinfo-tools
 module load htseq
 
-bam_dir=/home/yutkueri/private/alignment/paired_end
-gff_file=/home/yutkueri/private/annotation/pacbio_assembly_anno.gff
-outfile=/home/yutkueri/private/deg/rnaseq_counts.txt
+bam_dir=/home/yutkueri/private/alignment/paired_end/
+gff_file=/home/yutkueri/private/deg/pacbio_assembly_anno_no_def.gff
+outdir=/home/yutkueri/private/deg
 
-htseq-count -f bam alignment/paired_end/ERR1797969_Serum_aligned.bam alignment/paired_end/ERR1797972_BH_aligned.bam alignment/paired_end/ERR1797970_Serum_aligned.bam alignment/paired_end/ERR1797973_BH_aligned.bam alignment/paired_end/ERR1797971_Serum_aligned.bam alignment/paired_end/ERR1797974_BH_aligned.bam ${gff_file}
+for bamfile in ${bam_dir}*.bam
+do
+sample_name="${bamfile:44}"
+htseq-count -f bam -t CDS -i locus_tag ${bamfile} ${gff_file} > ${outdir}/${sample_name}.readcounts.txt
+done
