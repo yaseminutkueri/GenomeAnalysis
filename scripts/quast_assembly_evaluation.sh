0c5efa1c56637e3e332998722fcc4aac2a6656de
yaseminutkueri <yaseminutkueri@sabanciuniv.edu>
2023-04-12 15:36:12 +0200

    quast script updated to include reference genome

--- scripts/quast_assembly_evaluation.sh
+++ scripts/quast_assembly_evaluation.sh
@@ -13,7 +13,8 @@
 module load bioinfo-tools
 module load quast/5.0.2
 
-assembly_file=/home/yutkueri/private/assembly/pacbio.contigs.fasta
+assembly_file=/home/yutkueri/private/assembly/canu_assembly/pacbio.contigs.fasta
 out_dir=/home/yutkueri/private/quality_checks/canu_assembly
+reference=/home/yutkueri/private/raw_data/reference.fasta
 
-quast.py ${assembly_file} -o ${out_dir} 
+quast.py ${assembly_file} -r ${reference} -o ${out_dir} 
