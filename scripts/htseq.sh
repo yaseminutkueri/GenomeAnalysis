bd73b983ac5db2e8b079dd2febda2798ea58a811
yaseminutkueri <yaseminutkueri@sabanciuniv.edu>
2023-04-27 15:16:54 +0200

    differential expression - htseq counts

--- scripts/htseq.sh
+++ scripts/htseq.sh
@@ -2,10 +2,7 @@
 #SBATCH -A uppmax2023-2-8
 #SBATCH -M snowy
 #SBATCH -p core -n 1
-#SBATCH -t 10:00:00
-#SBATCH -o /home/yutkueri/private/slurm_messages/deg.out
-#SBATCH -e /home/yutkueri/private/slurm_messages/deg.err
-#SBATCH -J deg
+#SBATCH -t 01:00:00
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user yasemin.utkueri.3540@student.uu.se
 
@@ -17,8 +14,9 @@ bam_dir=/home/yutkueri/private/alignment/paired_end/
 gff_file=/home/yutkueri/private/deg/pacbio_assembly_anno_no_def.gff
 outdir=/home/yutkueri/private/deg
 
-for bamfile in ${bam_dir}*.bam
-do
+bamfile=$1
+
 sample_name="${bamfile:44}"
 htseq-count -f bam -t CDS -i locus_tag ${bamfile} ${gff_file} > ${outdir}/${sample_name}.readcounts.txt
-done
+
+
