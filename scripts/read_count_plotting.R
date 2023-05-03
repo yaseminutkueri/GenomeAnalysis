3822e9307f018b72ac1cd98ebd45c1c8540e6885
yaseminutkueri <yaseminutkueri@sabanciuniv.edu>
2023-05-03 16:14:47 +0200

    read count plot script

--- scripts/read_count_plotting.R
+++ scripts/read_count_plotting.R
@@ -15,3 +15,23 @@ colnames(counts) = c("ID", "ERR1797969_Serum", "ERR1797970_Serum", "ERR1797971_S
 
 melted_counts = melt(counts)
 ggplot(melted_counts, aes(x = melted_counts$value)) + geom_histogram(binwidth = 10000, fill = "purple") + facet_wrap(melted_counts$variable) + scale_y_log10() + xlab("Gene counts") + ylab("Frequency")
+
+
+BiocManager::install("GenomicFeatures")
+library(GenomicFeatures)
+txdb <- makeTxDbFromGFF(file = "/Users/yaseminutkueri/Desktop/GenomeAnalysis/differential_expression_analysis/pacbio_assembly_anno_no_def.gff", format = "gff3")
+
+library(ggplot2)
+# group the exons by transcript
+exons <- cdsBy(txdb, by = c("tx"))
+exons
+# make a data frame with transcripts and exon count
+exons.tx <- data.frame(tx = 1:length(exons), exons = sapply(exons, length))
+# plot
+ggplot(exons.tx) + geom_histogram(aes(exons), fill = "blue") + theme_bw()
+
+
+
+
+
+
